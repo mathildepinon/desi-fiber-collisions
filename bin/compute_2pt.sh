d086019fd22528627081b61dae73fc8120ed8feb
Mathilde Pinon <mth.pinon@gmail.com>
2024-02-27 01:25:34 -0800

    theta-cut with s-binning

--- bin/compute_2pt.sh
+++ bin/compute_2pt.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+#SBATCH --qos=regular
+#SBATCH --nodes=1
+#SBATCH --time=01:20:00
+#SBATCH --constraint=cpu
+#SBATCH --output='/global/u2/m/mpinon/_sbatch/power_thetacut.log'
 
-# Cubic mocks
-for i in {0..24}
-do
-    srun -n 128 python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/compute_2pt_clustering.py --todo 'power' --data_shortname 'cubic' --imock ${i} --tracer 'ELG' --z 1.325
-done
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+
+######################
+# Y1 SecondGen mocks #
+######################
+
+
+# run theta-cut with 3 methods
+srun -n 128 python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/mock_2pt_clustering.py --todo 'power' --mockgen 'second' --version 'v4_1' --imock 0 --tracer 'ELG_LOP' --region 'SGC' --completeness 'complete' --zmin 1.1 --zmax 1.6 --nrandoms 4 --thetacut 0.05 --direct True --directmax 100
+
+srun -n 128 python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/mock_2pt_clustering.py --todo 'power' --mockgen 'second' --version 'v4_1' --imock 0 --tracer 'ELG_LOP' --region 'SGC' --completeness 'complete' --zmin 1.1 --zmax 1.6 --nrandoms 4 --thetacut 0.05 --direct True --directmax 5000
+
+srun -n 128 python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/mock_2pt_clustering.py --todo 'power' --mockgen 'second' --version 'v4_1' --imock 0 --tracer 'ELG_LOP' --region 'SGC' --completeness 'complete' --zmin 1.1 --zmax 1.6 --nrandoms 4 --thetacut 0.05 --direct ''
