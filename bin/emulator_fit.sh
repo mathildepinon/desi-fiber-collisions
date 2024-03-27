55391b586c514f08fcff8007e96da37942ce5da0
Mathilde Pinon <mth.pinon@gmail.com>
2024-03-27 14:18:22 -0700

    commands to run fits

--- bin/emulator_fit.sh
+++ bin/emulator_fit.sh
@@ -1,22 +1,68 @@
 #!/bin/bash
 
-#source /global/cfs/cdirs/desi/users/adematti/cosmodesi_environment.sh main
-source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+#source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 
+######################
+# Y1 SecondGen mocks #
+######################
 
-## SecondGen cutsky mocks
-#srun -n 33 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'emulator' --source 'desi' --catalog "second" --version 'v3' --power True --tracer 'ELG_LOP'
+#########
+# Power #
+#########
 
-#srun -n 33 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'profiling' --source 'desi' --catalog "second" --version 'v3' --power True --tracer 'ELG_LOP' --region 'SGC' --completeness True --theta_cut 0.05 --sculpt_window True
+#srun -n 8 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'emulator' --source 'desi' --catalog "second" --version 'v4_1' --observable 'power' --tracer 'ELG_LOP' --zmin 1.1 --zmax 1.6 --z 1.325
 
-#srun -n 33 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'sampling' --source 'desi' --catalog "second" --version 'v3' --power True --tracer 'ELG_LOP' --region 'SGC' --completeness True --theta_cut 0.05
+#srun -n 8 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'sampling' --source 'desi' --catalog "second" --version 'v4_1' --observable 'power' --tracer 'ELG_LOP' --region 'GCcomb'  --zmin 1.1 --zmax 1.6 --z 1.325 --completeness 'complete' --covtype 'ezmocks'
 
-#srun -n 33 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'sampling' --source 'desi' --catalog "second" --version 'v3' --power True --tracer 'ELG_LOP' --region 'SGC' --completeness True --theta_cut 0.05 --sculpt_window True
+srun -n 8 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'sampling' --source 'desi' --catalog "second" --version 'v4_1' --observable 'power' --tracer 'ELG_LOP' --region 'SGC'  --zmin 1.1 --zmax 1.6 --z 1.325 --completeness 'complete' --thetacut 0.05 --covtype 'ezmocks'
 
-## Cubic mocks
+#for i in {0..24}
+#do
+#    srun -n 8 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'importance' --source 'desi' --catalog "second" --version 'v4_1' --observable 'power' --tracer 'ELG_LOP' --region 'SGC'  --zmin 1.1 --zmax 1.6 --z 1.325 --completeness 'complete' --imock $i
+#    srun -n 8 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'importance' --source 'desi' --catalog "second" --version 'v4_1' --observable 'power' --tracer 'ELG_LOP' --region 'SGC'  --zmin 1.1 --zmax 1.6 --z 1.325 --completeness 'complete' --thetacut 0.05 --imock $i
+#done
+
+#srun -n 8 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'emulator' --source 'desi' --catalog "second" --version 'v4_1' --observable 'power' --tracer 'ELG_LOPnotqso' --region 'SGC'  --zmin 1.1 --zmax 1.6 --z 1.325 --completeness 'altmtl'
+
+#srun -n 8 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'sampling' --source 'desi' --catalog "second" --version 'v4_1' --observable 'power' --tracer 'ELG_LOPnotqso' --region 'GCcomb'  --zmin 1.1 --zmax 1.6 --z 1.325 --completeness 'altmtl'
+
+#srun -n 8 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'sampling' --source 'desi' --catalog "second" --version 'v4_1' --observable 'power' --tracer 'ELG_LOPnotqso' --region 'GCcomb'  --zmin 1.1 --zmax 1.6 --z 1.325 --completeness 'altmtl' --thetacut 0.05
+
+#srun -n 8 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'profiling' --source 'desi' --catalog "second" --version 'v4_1' --observable 'power' --tracer 'ELG_LOP' --region 'SGC' --zmin 1.1 --zmax 1.6 --z 1.325 --completeness 'complete' --thetacut 0.05 --sculpt_window True --fixed_sn True --covtype 'ezmocks'
+
+#srun -n 8 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'sampling' --source 'desi' --catalog "second" --version 'v4_1' --observable 'power' --tracer 'ELG_LOP' --region 'SGC' --zmin 1.1 --zmax 1.6 --z 1.325 --completeness 'complete' --thetacut 0.05 --sculpt_window True  
+
+#srun -n 8 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'sampling' --source 'desi' --catalog "second" --version 'v4_1' --observable 'power' --tracer 'ELG_LOP' --region 'SGC' --zmin 1.1 --zmax 1.6 --z 1.325 --completeness 'complete' --thetacut 0.05 --sculpt_window True --systematic_priors 1 --covtype 'ezmocks'
+
+########
+# Corr #
+########
+
+#srun -n 33 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'emulator' --source 'desi' --catalog "second" --version 'v4_1' --observable 'corr' --tracer 'ELG_LOP' --zmin 1.1 --zmax 1.6 --z 1.325
+
+#srun -n 33 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'sampling' --source 'desi' --catalog "second" --version 'v4_1' --observable 'corr' --tracer 'ELG_LOP' --region 'SGC'  --zmin 1.1 --zmax 1.6 --z 1.325 --completeness 'complete'
+
+#srun -n 33 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'sampling' --source 'desi' --catalog "second" --version 'v4_1' --observable 'corr' --tracer 'ELG_LOP' --region 'SGC'  --zmin 1.1 --zmax 1.6 --z 1.325 --completeness 'complete' --thetacut 0.05
+
+#srun -n 33 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'emulator' --source 'desi' --catalog "second" --version 'v4_1' --observable 'corr' --tracer 'ELG_LOPnotqso' --zmin 1.1 --zmax 1.6 --z 1.325 --completeness 'altmtl'
+
+#srun -n 33 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'sampling' --source 'desi' --catalog "second" --version 'v4_1' --observable 'corr' --tracer 'ELG_LOPnotqso' --region 'GCcomb'  --zmin 1.1 --zmax 1.6 --z 1.325 --completeness 'altmtl'
+
+#srun -n 33 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'sampling' --source 'desi' --catalog "second" --version 'v4_1' --observable 'corr' --tracer 'ELG_LOPnotqso' --region 'GCcomb'  --zmin 1.1 --zmax 1.6 --z 1.325 --completeness 'altmtl' --thetacut 0.05
+
+#for i in {17..24}
+#do
+#    srun -n 33 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'importance' --source 'desi' --catalog "second" --version 'v4_1' --observable 'corr' --tracer 'ELG_LOP' --region 'SGC'  --zmin 1.1 --zmax 1.6 --z 1.325 --completeness 'complete' --imock $i
+#    srun -n 33 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'importance' --source 'desi' --catalog "second" --version 'v4_1' --observable 'corr' --tracer 'ELG_LOP' --region 'SGC'  --zmin 1.1 --zmax 1.6 --z 1.325 --completeness 'complete' --thetacut 0.05 --imock $i
+#done
+
+###############
+# Cubic mocks #
+###############
+
 #srun -n 33 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'emulator' --source 'local' --catalog "cubic" --tracer 'ELG' --power True --redshift 0.95
 #srun -n 33 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'profiling' --source 'local' --catalog "cubic" --tracer 'ELG' --power True --redshift 0.95
 #srun -n 33 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'emulator' --source 'local' --catalog "cubic" --tracer 'ELG' --power True --redshift 1.1
 #srun -n 33 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'profiling' --source 'local' --catalog "cubic" --tracer 'ELG' --power True --redshift 1.1
-srun -n 33 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'emulator' --source 'local' --catalog "cubic" --tracer 'ELG' --power True --redshift 1.325
-srun -n 33 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'profiling' --source 'local' --catalog "cubic" --tracer 'ELG' --power True --redshift 1.325
+#srun -n 33 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'emulator' --source 'desi' --catalog "cubic" --version 'v1.1' --tracer 'ELG' --observable 'power' --z 1.325 --zmin 1.1 --zmax 1.6
+#srun -n 33 --cpu_bind=cores python /global/u2/m/mpinon/fiber_collisions/desi_fiber_collisions/bin/emulator_fit.py --todo 'profiling' --source 'desi' --catalog "cubic" --version 'v1.1' --tracer 'ELG' --observable 'power' --z 1.325 --zmin 1.1 --zmax 1.6
