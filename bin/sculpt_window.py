solve bug with mmo term in rotation

bin/sculpt_window.py
@@ -5,7 +5,11 @@ import numpy as np
 import scipy.linalg as sla
 import time
 
-from jax.config import config; config.update('jax_enable_x64', True)
+try:
+    from jax import numpy as jnp
+    from jax.config import config; config.update('jax_enable_x64', True)
+except ImportError:
+    jnp = np
 
 from pypower import BaseMatrix, PowerSpectrumSmoothWindowMatrix, CatalogFFTPower, PowerSpectrumStatistics, PowerSpectrumMultipoles
 import anotherpipe.powerestimation.rotatewindow as rw
@@ -14,7 +18,6 @@ from desipipe.file_manager import BaseFile
 
 from utils import load_poles
 from cov_utils import truncate_cov, get_EZmocks_covariance
-from wmatrix_utils import compute_wmatrix
 from local_file_manager import LocalFileName
 from desi_file_manager import DESIFileName
 
@@ -96,8 +99,8 @@ def get_data(source='desi', catalog='second', version='v3', tracer='ELG', region
     zmax = zrange[1]
     
     if source == 'desi':
-        wm_fn = DESIFileName().set_default_config(version=version, ftype='wmatrix_smooth', tracer=tracer, region=region, completeness=completeness, realization='merged', rpcut=rpcut, thetacut=thetacut)
-        pk_fn = DESIFileName().set_default_config(version=version, ftype='pkpoles', tracer=tracer, region=region, completeness=completeness, rpcut=rpcut, thetacut=thetacut)
+        wm_fn = DESIFileName().set_default_config(version=version, ftype='wmatrix_smooth', tracer=tracer, region=region, completeness=completeness, realization='merged', rpcut=rpcut, thetacut=thetacut, baseline=False, weighting='_default_FKP_lin', nran=18, cellsize=6, boxsize=9000)
+        pk_fn = DESIFileName().set_default_config(version=version, ftype='pkpoles', tracer=tracer, region=region, completeness=completeness, rpcut=rpcut, thetacut=thetacut, baseline=False, weighting='_default_FKP_lin', nran=18, cellsize=6, boxsize=9000)
 
     elif source == 'local':
         wm_fn = LocalFileName().set_default_config(mockgen=catalog, ftype='wmatrix_smooth', tracer=tracer, region=region, completeness=completeness, realization=0 if catalog=='first' else None, rpcut=rpcut, thetacut=thetacut, directedges=(bool(rpcut) or bool(thetacut)))
@@ -108,6 +111,7 @@ def get_data(source='desi', catalog='second', version='v3', tracer='ELG', region
 
     # Window matrix
     wm_fn.update(zrange=zrange)
+    print(wm_fn.get_path())
     wm = PowerSpectrumSmoothWindowMatrix.load(wm_fn.get_path())
     w = wm.deepcopy()
     ktmin = w.xout[0][0]/2
@@ -122,18 +126,25 @@ def get_data(source='desi', catalog='second', version='v3', tracer='ELG', region
     pk.select(kolim).slice(slice(0, len(pk.k) // korebin * korebin, korebin))
     
     # Covariance matrix
-    cov_fn = '/global/cfs/cdirs/desi/users/mpinon/Y1/cov/{}cov_gaussian_pre_{}_{}_{:.1f}_{:.1f}_default_FKP_lin.txt'.format('noisy_' if 'noisy' in covtype else '', tracer, region, zmin, zmax)
+    cov_fn = '/global/cfs/cdirs/desi/users/mpinon/Y1/cov/pk/{}cov_gaussian_pre_{}_{}_{:.1f}_{:.1f}_default_FKP_lin.txt'.format('noisy_' if 'noisy' in covtype else '', tracer, region, zmin, zmax)
+    print(cov_fn)
     cov = np.loadtxt(cov_fn)
     cov = truncate_cov(cov, kinit=np.arange(0., 0.4, 0.005), kfinal=np.arange(kolim[0], kolim[1], 0.005))
     
     if covtype == 'ezmocks':
-        cov = get_EZmocks_covariance(stat='pkpoles', tracer=tracer, region=region, zrange=zrange, completeness='ffa', ells=(0, 2, 4), rpcut=rpcut, thetacut=thetacut, rebin=korebin, return_x=False)
-        cov = truncate_cov(cov, kinit=np.arange(0., 0.520, 0.005), kfinal=np.arange(kolim[0], kolim[1], 0.005))
+        cov_fn = '/global/cfs/cdirs/desi/users/mpinon/Y1/cov/pk/cov_EZmocks_{}_ffa_{}_z{:.3f}-{:.3f}_k{:.2f}-{:.2f}{}.npy'.format(tracer[:7], region, zmin, zmax, kolim[0], kolim[1], '_thetacut{:.2f}'.format(thetacut) if thetacut else '')
+        print(cov_fn)
+        if not os.path.isfile(cov_fn):
+            cov = get_EZmocks_covariance(stat='pkpoles', tracer=tracer, region=region, zrange=zrange, completeness='ffa', ells=(0, 2, 4), select=(kolim[0], kolim[1], 0.005), rpcut=rpcut, thetacut=thetacut, return_x=False)
+            np.save(cov_fn, cov)
+        else:
+            print('Loading EZmocks covariance: {}'.format(cov_fn))
+            cov = np.load(cov_fn)
 
     return {'power': pk, 'wmatrix': w, 'covariance': cov}
 
 
-def rotate_data(ells=[0, 2,4], capsig=1000, difflfac=100, save=True, **kwargs):
+def rotate_data(ells=[0, 2,4], capsigW=1000, capsigR=1000, difflfac=100, csub=True, save=True, **kwargs):
     
     t0 = time.time()
     
@@ -141,7 +152,7 @@ def rotate_data(ells=[0, 2,4], capsig=1000, difflfac=100, save=True, **kwargs):
     data_processed = pe.make(data['power'], data['wmatrix'], data['covariance'])
 
     # Fit
-    mmatrix, state = rw.fit(data_processed, ls=ells, momt=max(kwargs['rpcut'], kwargs['thetacut']), capsig=capsig, difflfac=difflfac)
+    mmatrix, state = rw.fit(data_processed, ls=ells, momt=max(kwargs['rpcut'], kwargs['thetacut']), csub=csub, capsigW=capsigW, difflfac=difflfac, capsigR=capsigR)
 
     # Rotated data
     rotated_data = pe.rotate(data_processed, mmatrix, ls=ells)
@@ -151,9 +162,13 @@ def rotate_data(ells=[0, 2,4], capsig=1000, difflfac=100, save=True, **kwargs):
     wmatrixnew.value = np.array(rotated_data.W(ls=ells)).T
     covnew = rotated_data.C(ls=ells)
     mo = rotated_data.mo
-    if len(mmatrix) == 4:
+
+    if len(mmatrix)==4:
         mt = mmatrix[2]
         m = mmatrix[3]
+    elif len(mmatrix)==3:
+        mt = mmatrix[2]
+        m = None
     else:
         mt = None
         m = None
@@ -162,17 +177,19 @@ def rotate_data(ells=[0, 2,4], capsig=1000, difflfac=100, save=True, **kwargs):
 
     if save:
         output_dir = "/global/cfs/cdirs/desi/users/mpinon/secondGenMocksY1/{}/sculpt_window".format(kwargs['version'])        
-        output_fn = LocalFileName().set_default_config(ftype='sculpt_all', tracer=kwargs['tracer'], region=kwargs['region'], completeness=kwargs['completeness'], realization=None, weighting=None, rpcut=kwargs['rpcut'], thetacut=kwargs['thetacut'])
+        output_fn = LocalFileName().set_default_config(ftype='rotated_all', tracer=kwargs['tracer'], region=kwargs['region'], completeness=kwargs['completeness'], realization=None, weighting=None, rpcut=kwargs['rpcut'], thetacut=kwargs['thetacut'])
         output_fn.update(fdir=output_dir, zrange=kwargs['zrange'], cellsize=None, boxsize=None, directedges=False)
-        output_fn.sculpt_attrs['ells'] = ells
-        output_fn.sculpt_attrs['kobsmax'] = kwargs['kolim'][-1]
-        output_fn.sculpt_attrs['ktmax'] = kwargs['ktmax']
-        output_fn.sculpt_attrs['capsig'] = capsig
-        output_fn.sculpt_attrs['difflfac'] = difflfac
-        output_fn.sculpt_attrs['covtype'] = kwargs['covtype']
+        output_fn.rotation_attrs['ells'] = ells
+        output_fn.rotation_attrs['kobsmax'] = kwargs['kolim'][-1]
+        output_fn.rotation_attrs['ktmax'] = kwargs['ktmax']
+        output_fn.rotation_attrs['max_sigma_W'] = capsigW
+        output_fn.rotation_attrs['max_sigma_R'] = capsigR
+        output_fn.rotation_attrs['factor_diff_ell'] = difflfac
+        output_fn.rotation_attrs['covtype'] = kwargs['covtype']
+        output_fn.rotation_attrs['csub'] = csub
         
-        sculpt_window = SculptWindow(wmatrix=data['wmatrix'], pk=data['power'], cov=data['covariance'], mmatrix=mmatrix[0], mo=mo, mt=mt, m=m, wmatrixnew=wmatrixnew, pknew=np.array(pknew), covnew=covnew)
-        sculpt_window.save(output_fn.get_path())
+        rotated_window = SculptWindow(wmatrix=data['wmatrix'], pk=data['power'], cov=data['covariance'], mmatrix=mmatrix[0], mo=mo, mt=mt, m=m, wmatrixnew=wmatrixnew, pknew=np.array(pknew), covnew=covnew)
+        rotated_window.save(output_fn.get_path())
         
     print('Elapsed time: {:.2f} s'.format(time.time() - t0))
 
@@ -180,11 +197,11 @@ def rotate_data(ells=[0, 2,4], capsig=1000, difflfac=100, save=True, **kwargs):
 if __name__ == '__main__':
     source = 'desi' # desi or local
     catalog = 'second' # first, second, or data
-    version = 'v3'
+    version = 'v4_1'
 
     tracer = "ELG_LOPnotqso"
     region = "SGC"
-    zrange = (0.8, 1.1)
+    zrange = (1.1, 1.6)
     completeness = True
     
     ls = [0, 2, 4]
@@ -197,9 +214,11 @@ if __name__ == '__main__':
     rpcut = 0.
     thetacut = 0.05
     
-    capsig = 5
+    capsigR = 2
+    capsigW = 5
     difflfac = 10
-    covtype = 'noisyanalytic'
+    csub = True
+    covtype = 'ezmocks'
     
-    rotate_data(source=source, catalog=catalog, version=version, tracer=tracer, region=region, zrange=zrange, completeness=completeness, rpcut=rpcut, thetacut=thetacut, ells=ls, kolim=kolim, korebin=korebin, ktmax=ktmax, ktrebin=ktrebin, save=True, capsig=capsig, difflfac=difflfac, covtype=covtype)
+    rotate_data(source=source, catalog=catalog, version=version, tracer=tracer, region=region, zrange=zrange, completeness=completeness, rpcut=rpcut, thetacut=thetacut, ells=ls, kolim=kolim, korebin=korebin, ktmax=ktmax, ktrebin=ktrebin, save=True, capsigW=capsigW, capsigR=capsigR, difflfac=difflfac, covtype=covtype, csub=csub)
 

bin/wmatrix_utils.py
@@ -8,6 +8,8 @@ def plot_matrix(mat, x1=None, x2=None, xlabel1=None, xlabel2=None, barlabel=None
                 corrcoef=False, figsize=None, norm=None, labelsize=None):
 
     size1, size2 = [row[0].shape[0] for row in mat], [col.shape[1] for col in mat[0]]
+    
+    from mpl_toolkits.axes_grid1 import make_axes_locatable
 
     def _make_list(x, size):
         if not utils.is_sequence(x):
@@ -35,8 +37,6 @@ def plot_matrix(mat, x1=None, x2=None, xlabel1=None, xlabel2=None, barlabel=None
                             figsize=(figsize[0] / xextend, figsize[1]),
                             gridspec_kw={'width_ratios': size1, 'height_ratios': size2[::-1]},
                             squeeze=False)
-    wspace = hspace = 0.18
-    fig.subplots_adjust(wspace=wspace, hspace=hspace)
     for i in range(ncols):
         for j in range(nrows):
             ax = lax[nrows - j - 1][i]
@@ -45,21 +45,20 @@ def plot_matrix(mat, x1=None, x2=None, xlabel1=None, xlabel2=None, barlabel=None
             if x2[j] is None: xx2 = 1 + np.arange(mat[i][j].shape[1])
             mesh = ax.pcolor(xx1, xx2, mat[i][j].T, norm=norm, cmap=plt.get_cmap('RdBu'))
             if i > 0 or x1[i] is None: ax.yaxis.set_visible(False)
-            if j == 0 and xlabel1[i]: ax.set_xlabel(xlabel1[i], fontsize=labelsize)
+            if j == 0 and xlabel1[i]: ax.set_xlabel(xlabel1[i])
             if j > 0 or x2[j] is None: ax.xaxis.set_visible(False)
-            if i == 0 and xlabel2[j]: ax.set_ylabel(xlabel2[j], fontsize=labelsize)
+            if i == 0 and xlabel2[j]: ax.set_ylabel(xlabel2[j])
             ax.tick_params()
             if label1[i] is not None or label2[j] is not None:
                 text = r'{} $\times$ {}'.format(label1[i], label2[j])
-                ax.text(0.05, 0.95, text, horizontalalignment='left', verticalalignment='top',\
-                        transform=ax.transAxes, color='black')
+                ax.text(0.05, 0.95, text, horizontalalignment='left', verticalalignment='top', transform=ax.transAxes, color='black', fontsize=12)
             ax.grid(False)
-
-    fig.subplots_adjust(right=xextend)
-    cbar_ax = fig.add_axes([xextend + 0.2, 0.11, 0.03, 0.86])
-    cbar_ax.tick_params()
+            
+    plt.subplots_adjust(wspace=0.1, hspace=0.1, left=0.15, bottom=0.15, right=0.8)
+    cbar_ax = fig.add_axes([xextend+0.025, 0.15, 0.02, 0.8])
     cbar = fig.colorbar(mesh, cax=cbar_ax)
-    if barlabel: cbar.set_label(barlabel, rotation=90)
+    if barlabel: cbar.set_label(barlabel)
+    fig.align_ylabels()
     return lax
 
 # compute window matrix from window power